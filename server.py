102c660d78450645234994df7f52fabba7c87ef6
reindeersled <reina.lin26@trinityschoolnyc.org>
2024-09-05 13:00:10 -0400

    ok

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 # flask --app data_server run
 from flask import Flask
 from flask import render_template
-import json
+from flask import request
+import os
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
-    f = open("data/Excel_dataset.json", "r")
-    data = json.load(f)
 
-    f.close()
-
-    return render_template('/login.html')
+    return render_template('/login.html', )
 
 @app.route('/game')
 def about():
+    username = request.args.get('username')
     
-    return render_template('/game.html')
+    return render_template('/game.html', username=username)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, port=port)
